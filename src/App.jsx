e87ce1e15d7bea9e7ca0a2e071bbcf2f4e528800
rajkumargithub4 <143372039+rajkumargithub4@users.noreply.github.com>
2024-08-07 15:13:59 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-
+import Navbar from "./Components/Navbar";
 function App() {
 
   return (
     <>
-     <h1>rajkumar</h1>
+     <h1>Navbar</h1>
+     <h1>Navbar</h1>
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Navbar() {
+
+  return (
+    <>
+     <h1>rajkumar</h1>
+    </>
+  )
+}
+
+export default Navbar
